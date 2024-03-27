7f6fb3206437f268cfb85bdbbc211c110e8a1c69
Matt <matt@matts-air-2.lan>
2024-03-27 10:42:37 -0500

    whoops

--- main.go
+++ main.go
@@ -71,3 +71,13 @@ func getAlbumByID(c *gin.Context) {
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
+
+// Add is our function that sums two integers
+func Add(x, y int) (res int) {
+	return x + y
+}
+
+// Subtract subtracts two integers
+func Subtract(x, y int) (res int) {
+	return x - y
+}
